af964017404b352ff5af0c03984b5bd282735dad
Michael Nguyen <ngxphong03@gmail.com>
2025-03-03 17:46:38 +0700

    dựng base project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
