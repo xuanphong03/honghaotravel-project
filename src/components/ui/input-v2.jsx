9eb86ae74c535598b1567341324904a43b56270b
NgocMinh10122001 <phamngocminh10122001@gmail.com>
2025-03-18 14:08:54 +0700

    fix shadcn ui

--- src/components/ui/input-v2.jsx
+++ src/components/ui/input-v2.jsx
@@ -20,6 +20,23 @@ function Input({
       {...props} />)
   );
 }
+function InputCustom({
+  className,
+  type,
+  ...props
+}) {
+  return (
+    (<input
+      type={type}
+      data-slot="input"
+      className={cn(
+        "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0  border bg-transparent px-3 py-1 text-base  outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed  md:text-sm",
+        "",
+        "",
+        className
+      )}
+      {...props} />)
+  );
+}
 
-
-export { Input }
+export { Input, InputCustom }
--- src/components/ui/textarea-v2.jsx
+++ src/components/ui/textarea-v2.jsx
@@ -16,6 +16,19 @@ function Textarea({
       {...props} />)
   );
 }
+function TextareaCustom({
+  className,
+  ...props
+}) {
+  return (
+    (<textarea
+      data-slot="textarea"
+      className={cn(
+        " placeholder:text-muted-foreground   flex field-sizing-content min-h-16 w-full  border bg-transparent px-3 py-2 text-base  outline-none  disabled:cursor-not-allowed  md:text-sm",
+        className
+      )}
+      {...props} />)
+  );
+}
 
-
-export { Textarea }
+export { Textarea,TextareaCustom }
